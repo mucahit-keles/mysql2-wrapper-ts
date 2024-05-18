a2cb45dd2b8faa78f1a6a901944dbc9f236305d5
mucahit-keles <131585878+mucahit-keles@users.noreply.github.com>
2024-05-18 09:35:57 +0300

    Add a method to insert multiple columns at once (with optional support for adding it before or after another column)

--- sql_wrapper.ts
+++ sql_wrapper.ts
@@ -12,7 +12,11 @@ export async function getWrapper(connectionOptions: ConnectionOptions, timeZone?
 
 type IQueryValue = string | number | Date;
 type IDBColumn = { Field: string };
-type ICountValue = { count: number };
+
+function mysql_real_escape_string(unsafeString: string): string {
+	const safeString: string = dbConnection.escape(unsafeString);
+	return safeString.substring(1, safeString.length - 1);
+}
 
 export class IWrapper {
 	public async getData(query: string, params?: IQueryValue[]): Promise<RowDataPacket[]> {
@@ -27,17 +31,55 @@ export class IWrapper {
 			const rows: RowDataPacket[] = await this.getData(`${query} LIMIT 1`, params);
 			return rows.length === 0 ? undefined : rows[0];
 		} catch (e: any) {
-			throw new Error(`getOneData Exception: ${e.message.replace("getData Exception: ", "")}`);
+			throw new Error(e.message.replace("getData Exception:", "getOneData Exception:"));
 		}
 	};
 	public async getColumns(tableName: string, excludedColumns?: string[]): Promise<string[]> {
-		let excludedColumns_params: Array<"?"> | undefined = excludedColumns ? new Array(excludedColumns.length).fill("?") : undefined;
-		let excludedColumns_params_csv: string | undefined = excludedColumns_params ? excludedColumns_params.join(",") : undefined;
-		
-		const columns_queryParams: string = excludedColumns_params_csv ? ` WHERE \`Field\` NOT IN (${excludedColumns_params_csv})` : "";
-		const columns: IDBColumn[] = await this.getData(`SHOW COLUMNS IN \`${tableName}\`${columns_queryParams}`, excludedColumns) as IDBColumn[];
-		
-		return columns.map((column: IDBColumn): string => column.Field) as string[];
+		try {
+			tableName = mysql_real_escape_string(tableName);
+			
+			let excludedColumns_params: Array<"?"> | undefined = excludedColumns ? new Array(excludedColumns.length).fill("?") : undefined;
+			let excludedColumns_params_csv: string | undefined = excludedColumns_params ? excludedColumns_params.join(",") : undefined;
+			
+			const columns_queryParams: string = excludedColumns_params_csv ? ` WHERE \`Field\` NOT IN (${excludedColumns_params_csv})` : "";
+			const columns: IDBColumn[] = await this.getData(`SHOW COLUMNS IN \`${tableName}\`${columns_queryParams}`, excludedColumns) as IDBColumn[];
+			
+			return columns.map((column: IDBColumn): string => column.Field) as string[];
+		} catch (e: any) {
+			throw new Error(e.message.replace("getData Exception:", "getColumns Exception:"));
+		}
+	};
+	public async insertColumns(tableName: string, columns: string[] | string, insertAfter?: string, insertBefore?: string): Promise<void> {
+		try {
+			tableName = mysql_real_escape_string(tableName);
+			
+			if (typeof columns === "string") columns = [ columns ];
+			for (let columnIndex: number = 0; columnIndex < columns.length; columnIndex++) {
+				const column = columns[columnIndex];
+				columns[columnIndex] = mysql_real_escape_string(column);
+			}
+			columns.reverse();
+			
+			let insertColumns_queryParams: string = insertAfter  ? ` AFTER ${insertAfter}` : "";
+			if (insertBefore) {
+				const columns: string[] = await this.getColumns(tableName);
+				let insertAfter_found;
+				for (let columnIndex: number = 0; columnIndex < columns.length; columnIndex++) {
+					const column: string = columns[columnIndex];
+					if (column === insertBefore) {
+						insertAfter_found = columns[columnIndex - 1];
+						insertColumns_queryParams = insertAfter_found  ? ` AFTER ${insertAfter_found}` : "";
+						break;
+					}
+				}
+			}
+			
+			const columns_csv: string = " ADD COLUMN `" + columns.join(`\` VARCHAR(255)${insertColumns_queryParams}, ADD COLUMN \``) + `\` VARCHAR(255)${insertColumns_queryParams}`;
+			
+			await this.execute(`ALTER TABLE \`${tableName}\`${columns_csv}`);
+		} catch (e: any) {
+			throw new Error(e.message.replace("execute Exception:", "insertColumns Exception:"));
+		}
 	};
 	public async execute(query: string, params?: IQueryValue): Promise<number> {
 		try {
